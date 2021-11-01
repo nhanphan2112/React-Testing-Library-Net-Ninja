7aeafe2f14a615b0ba22bc001d70faaddc7aac9b
Nhan Phan <nphanacc@gmail.com>
2021-11-01 14:46:28 -0700

    Using screen methods on Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
-export default function Header({
-    title
-}) {
-    return <h1 className="header">{title}</h1>
+export default function Header({ title }) {
+	return (
+		<>
+			{/*Line 8 data-testid is used for getById in Header.test.js  */}
+			<h1 className="header" data-testid="header-1">
+				{title}
+			</h1>
+			{/* Line 12 title is used to test getByTitle in Header.test.js */}
+			<h3 title="Header" className="header">
+				Cat
+			</h3>
+		</>
+	);
 }
---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+
+it("should render same text passed into title props", () => {
+	render(<Header title="My Header" />);
+	const headingElement = screen.getByText(/my header/i);
+	expect(headingElement).toBeInTheDocument();
+});
+
+it("making sure the heading is loaded using getByRole", () => {
+	render(<Header title="My Header" />);
+	const headingElement = screen.getByRole("heading", { name: "My Header" });
+	expect(headingElement).toBeInTheDocument();
+});
+
+// semantic quuery
+it("making sure the heading is loaded using getByTitle", () => {
+	render(<Header title="My Header" />);
+	const headingElement = screen.getByTitle("Header");
+	expect(headingElement).toBeInTheDocument();
+});
+
+// test Id method
+it("making sure the heading is loaded using getByTestId", () => {
+	render(<Header title="My Header" />);
+	const headingElement = screen.getByTestId("header-1");
+	expect(headingElement).toBeInTheDocument();
+});
+
+//-------------------------------FIND BY-----------------------------------------------
+it("Header test using findByText", async () => {
+	render(<Header title="My Header" />);
+	const headingElement = await screen.findByText(/my header/i);
+	expect(headingElement).toBeInTheDocument();
+});
+
+//-------------------------------QUERY BY-----------------------------------------------
+
+it("Header test using queryByText", () => {
+	render(<Header title="My Header" />);
+	const headingElement = screen.queryByText(/dogs/i);
+	expect(headingElement).not.toBeInTheDocument();
+	// Line 42 not mean that we are not expecting line 41 element to be in the DOM
+});
+
+//-------------------------------GET ALL BY-----------------------------------------------
+it("Headers test using getAllByRole", () => {
+	render(<Header title="My Header" />);
+	const headingElements = screen.getAllByRole("heading");
+	expect(headingElements.length).toBe(2);
+});
